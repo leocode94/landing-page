199b988470d140d19f04c94800a3dd4aa2b534ef
Leonardo Santana <leonardoba780@gmail.com>
2023-05-28 21:31:44 -0300

    Added Netlify's configuration

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,13 @@
 export default function Form() {
     return (
         <form className="form" name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
             <label className="form--label" htmlFor="name">Nome completo:</label><br />
-            <input className="form--input" type="text" id="name" name="name" required /><br />
+            <input className="form--input" type="text" id="name" name="name" required/><br />
             <label className="form--label" htmlFor="phone">Telefone:</label><br />
-            <input className="form--input" type="text" id="phone" name="phone" /><br />
+            <input className="form--input" type="text" id="phone" name="phone" required/><br />
             <label className="form--label" htmlFor="spending">Consumo:</label><br />
-            <input className="form--input" type="text" id="spending" name="spending"/><br />
+            <input className="form--input" type="text" id="spending" name="spending" required/><br />
             <input className="form--submit" type="submit" value="ENVIAR" />
         </form>
     );
