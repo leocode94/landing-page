841771d4f52a1572e96f0d039a690be9124949c4
Leonardo Santana <leonardoba780@gmail.com>
2023-09-23 21:54:30 -0300

    Removed radio buttons test

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -9,11 +9,6 @@ export default function Contato() {
                 <input className="form--input" type="text" id="phone" name="phone" required /><br />
                 <label className="form--label" htmlFor="city">Cidade:</label><br />
                 <input className="form--input" type="text" id="city" name="city" required /><br />
-                <h2>Radio?</h2>
-                <label className="form--label" htmlFor="radio-sim">Sim</label><br />
-                <input className="form--radio" type="radio" id="radio-sim" name="radio" value="Sim" /><br />
-                <label className="form--label" htmlFor="radio-nao">Não</label><br />
-                <input className="form--radio" type="radio" id="radio-nao" name="radio" value="Não" /><br />
                 <label className="form--label" htmlFor="message">Escreva uma mensagem:</label><br />
                 <textarea className="form--textarea" name="message" rows={8} cols={34} /><br />
                 <input className="form--submit" type="submit" value="ENVIAR" />
